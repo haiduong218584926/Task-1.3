95dc882d1f799e965241d85eb79da749e9856b97
haiduong218584926 <142541230+haiduong218584926@users.noreply.github.com>
2023-09-29 23:54:16 +1000

    Add files via upload

---
+++ ContactMeButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main-container">
+        <h1>
+            <a href="">
+                Contact me
+            </a>
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .main-container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+}
+    
+</style>
---
+++ ContactMePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="main-container">
+        <h1>
+            <a href="">
+                Contact me
+            </a>
+        </h1>
+    </div>
+</template>
+
